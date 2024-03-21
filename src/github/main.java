a77741f474dfcd8ddaa5e843cfb9da45b51f4291
GokcenYedidag <gokcenyedidag@gmail.com>
2024-03-21 17:47:39 +0300

    main v3

--- src/github/main.java
+++ src/github/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("main v1");
         System.out.println("main v2");
+        System.out.println("main v3");
     }
 }
